86043ac6a23ee36e92f8849404225ac7291ffeed
Mor Sela <morsela@gmail.com>
2016-12-25 20:14:58 +0200

    added getstatus

--- index.js
+++ index.js
@@ -12,15 +12,41 @@ app.set("view engine", "ejs");
 
 const ENABLE_CMD = "http://server.switcher.co.il/Switcher/appServiceSetSwitchState?token=1455239767592&switchId=1429959227412&state=on"
 const DISABLE_CMD = "http://server.switcher.co.il/Switcher/appServiceSetSwitchState?token=1455239767592&switchId=1429959227412&state=off"
+const ENABLE_DURATION = "http://server.switcher.co.il/Switcher/setSpontaneousEvent?token=1455239767592&switchId=1429959227412&isManual=true&duration=600000"
+const GET_STATE = "http://server.switcher.co.il/Switcher/appServiceGetSwitchState?token=1455239767592&switchId=1429959227412"
 
-var alexaApp = new alexa.app("test");
+var alexaApp = new alexa.app("SwitcherDud");
 alexaApp.launch(function(request, response) {
   response.say("You launched the app!");
 });
 
 alexaApp.dictionary = { "start_synonym": ["turn on", "start", "enable"], "stop_synonym": ["turn off", "stop", "disable"] };
 
-alexaApp.intent("EnableDud", {
+alexaApp.intent("GetDoodStatus", {
+    "slots": { },
+    "utterances": [
+      "status"
+    ]
+  },
+  function(req, res) {
+    request(GET_STATE, function (error, response, body) {
+      if (error || response.statusCode != 200) {
+        res.say("Cannot get state");
+      } else {
+        parsed_body = JSON.parse(body);
+        state = parsed_body["state"];
+
+        if (state == "on") {
+          res.say("Dood is on");
+        } else {
+          res.say("Dood is off");
+        }
+      }
+  })
+  }
+);
+
+alexaApp.intent("EnableDood", {
     "slots": { },
     "utterances": [
       "{start_synonym}"
@@ -29,15 +55,36 @@ alexaApp.intent("EnableDud", {
   function(req, res) {
   	request(ENABLE_CMD, function (error, response, body) {
   		if (error || response.statusCode != 200) {
-  			response.say("Dood was not turned on");
+  			res.say("Dood was not turned on");
   		} else {
-  			response.say("Dood was turned on successfully!");
+  			res.say("Dood was turned on successfully!");
   		}
 	})
   }
 );
 
-alexaApp.intent("DisableDud", {
+alexaApp.intent("EnableDoodWithDuration", {
+    "slots": {
+          "name": "Duration",
+          "type": "AMAZON.DURATION"
+        },
+    "utterances": [
+      "start for {Duration}"
+    ]
+  },
+  function(req, res) {
+    res.say("enable for duration");
+    // request(ENABLE_DURATION, function (error, response, body) {
+    //   if (error || response.statusCode != 200) {
+    //     response.say("Dood was not turned on");
+    //   } else {
+    //     response.say("Dood was turned on successfully!");
+    //   }
+  // })
+  }
+);
+
+alexaApp.intent("DisableDood", {
     "slots": { },
     "utterances": [
       "{stop_synonym}"
@@ -46,17 +93,15 @@ alexaApp.intent("DisableDud", {
   function(req, res)  {
   	request(DISABLE_CMD, function (error, response, body) {
   		if (error || response.statusCode != 200) {
-  			response.say("Dood was not turned off");
+  			res.say("Dood was not turned off");
   		} else {
-  			response.say("Dood was turned off successfully!");
+  			res.say("Dood was turned off successfully!");
   		}
 	})
   }
 );
 
 alexaApp.express(app, "/echo/");
-
-// launch /echo/test in your browser with a GET request
 
 app.listen(PORT);
 console.log("Listening on port " + PORT);
