f2f9a4e71c2ed6993f5a4b7f7103dd41e46773fa
Mor Sela <morsela@gmail.com>
2017-01-03 20:36:36 +0200

    added mixpanel for login functionality

--- login.js
+++ login.js
@@ -1,7 +1,12 @@
 var path = require('path');
 var util = require('util')
+var Mixpanel = require('mixpanel');
 
 var Switcher = require('./switcher')
+
+var mixpanel = Mixpanel.init(process.env.MIXPANEL_KEY, {
+    protocol: 'https'
+});
 
 module.exports = function(app) {
 	app.get('/echo/SwitcherDud/login/', function(req, res) {
@@ -14,12 +19,16 @@ module.exports = function(app) {
 
 	app.post('/echo/SwitcherDud/login/', function(req, res) {
 	    Switcher.login(req.body.username, req.body.password).then(accessToken => {
+	    	mixpanel.track('login_success', { ip: req.connection.remoteAddress });
+
 	        if (req.session.redirectURI != undefined) {
 	            res.redirect(util.format('%s#state=%s&access_token=%s&token_type=Bearer', req.session.redirectURI, req.session.state, accessToken))
 	        } else {
 	            res.send("Login success")
 	        }
 	    }).catch(err => {
+	    	mixpanel.track('login_failure', { ip: req.connection.remoteAddress });
+
 	    	console.error(err);
 
 	        res.send("login failed")
